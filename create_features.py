1b6212decd074f788e9583180d9fcd64d53b97a8
genp <gen@cs.brown.edu>
2016-02-26 16:44:13 -0500

    updated create_feature

--- create_features.py
+++ create_features.py
@@ -12,10 +12,10 @@ import scipy
 
 
 # model setup - this will have different names depending on the model you download. It could be deploy.prototxt or solver.prototxt
-MODEL_FILE = '/home/gen/caffe/models/hybridCNN/hybridCNN_deploy_FC7.prototxt'
+MODEL_FILE = ''
 
 # pretrained weights - this file will be called somthing.caffemodel
-PRETRAINED = '/home/gen/caffe/models/hybridCNN/hybridCNN_iter_700000.caffemodel'
+PRETRAINED = ''
 
 # Note: this feature normalization was recommended in Razavian et al. "Cnn features off-the-shelf: an astounding baseline for recognition."
 def norm_convnet_feat(feature):
@@ -48,7 +48,6 @@ def img_feat(img_names, net, transformer, save_path, type_name, layer_name, norm
         subdir = os.path.basename(img_name)[:2]
         if not os.path.exists(os.path.join(save_path, subdir)):
             os.makedirs(os.path.join(save_path, subdir))
-            os.makedirs(os.path.join(save_path, subdir, 'norm'))            
         sname = os.path.join(save_path, subdir, '{0}_{1}.jbl'.format(os.path.basename(img_name).split('.')[0], layer_name))
 
         # check if this feature was already calculated
@@ -70,8 +69,8 @@ def img_feat(img_names, net, transformer, save_path, type_name, layer_name, norm
         else:
             feat = norm_convnet_feat(out[layer_name])
 
-        # saving feature in joblib file format
-        joblib.dump(feat, sname)
+        # saving feature in joblib file format with compression to save space
+        joblib.dump(feat, sname, compress = 6)
 
     
 if __name__ == "__main__":
